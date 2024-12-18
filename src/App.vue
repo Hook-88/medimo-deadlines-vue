36e5b1d21dc62f563a8ca4219d1d573c978c7e71
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2024-12-18 11:21:52 -0500

    second commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div class="flex flex-col h-screen">
+    <header class="bg-white p-4 shadow-sm">
+      <h1 class="text-2xl">Medimo Deadlines</h1>
+      <!-- <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" /> -->
+
+      <!-- <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav> -->
+    </header>
+
+    <RouterView />
+
+    <footer class="text-center py-2">
+      <small>&copy; 2024 Hook88</small>
+    </footer>
+  </div>
+
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <main class="flex-grow">
+    here main
+  </main>
+</template>
